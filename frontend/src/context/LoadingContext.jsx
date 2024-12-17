0f13d9c751a6cb853d4ea542f4111dc9ba08b350
Divyam Chauhan <mi.divyam@gmail.com>
2024-12-17 16:12:32 +0530

    update

---
+++ frontend/src/context/LoadingContext.jsx
@@ -0,0 +1,25 @@
+// LoadingContext.jsx
+import React, { createContext, useContext, useState } from "react";
+import Loader from "../components/Loader/Loader";
+
+// Create a Context for the loading state
+const LoadingContext = createContext();
+
+// Create a custom hook to use the LoadingContext
+export const useLoading = () => useContext(LoadingContext);
+
+// Provider component
+export const LoadingProvider = ({ children }) => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const startLoading = () => setIsLoading(true);
+  const stopLoading = () => setIsLoading(false);
+
+  return (
+    <LoadingContext.Provider value={{ startLoading, stopLoading }}>
+      {console.log({ isLoading })}
+      {isLoading && <Loader />}
+      {children}
+    </LoadingContext.Provider>
+  );
+};
